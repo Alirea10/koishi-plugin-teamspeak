eb809ca2d64f39a33f9341f83cf2f5a177ab7b21
Alirea <2981883527@qq.com>
2025-10-18 17:28:09 +0800

    refactor: 将命令和配置拆分

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,96 @@
-import { Context, Schema } from 'koishi'
+import {Context, Logger,} from "koishi";
+import {
+  TeamSpeak,
+  QueryProtocol,
+  ClientConnectEvent,
+  ClientType
+} from "ts3-nodejs-library";
 
-export const name = 'teamspeak'
+import {Config} from "./config";
+import {registerCommands} from "./command";
 
-export interface Config {}
+export const inject = [ 'database' ]
+export * from "./config";
+export * from "./command";
+export const name = "teamspeak";
 
-export const Config: Schema<Config> = Schema.object({})
+export function apply(ctx: Context, config: Config) {
 
-export function apply(ctx: Context) {
-  // write your plugin here
+  const logger = new Logger("teamspeak");
+  const bot = ctx.bots[0];
+  let ts: TeamSpeak | null;
+
+
+  const joinListener = (e: ClientConnectEvent) => {
+    if (e.client.type === ClientType.ServerQuery) return;
+    const group=[...config.groups]
+    logger.debug(config.groups)
+    logger.info(`${e.client.nickname} 进入了TS.`);
+    ctx.broadcast(group,`${e.client.nickname} 进入了TS.`)
+  };
+
+  const closeListener = async () => {
+    logger.info("连接中断...");
+    try {
+      await ts.reconnect(-1, 1000);
+      logger.info("重连成功!");
+    } catch (error) {
+      logger.error("重连失败:", error);
+    }
+  };
+
+  const TextMessageListener = (e: any) => {
+    if (e.invoker.type === ClientType.ServerQuery) {
+      logger.info(e.msg)
+    }else{
+      logger.debug(config.groups)
+      logger.info(`${e.invoker.nickname} 在TS说: ${e.msg}`);
+      const group=[...config.groups]
+      ctx.broadcast(group,`${e.invoker.nickname} 在TS说: ${e.msg}`)
+    }
+  }
+
+
+  ctx.on("ready", async () => {
+    try {
+      ts = await TeamSpeak.connect({
+        host: config.host,
+        serverport: config.port,
+        protocol: config.protocol === "raw" ? QueryProtocol.RAW : config.protocol === "ssh" ? QueryProtocol.SSH : null,
+        queryport: config.queryport,
+        username: config.user,
+        password: config.password,
+        nickname: config.nickname,
+      });
+
+      logger.info("已连接到teamspeak服务器.");
+
+      ts.on("ready", async () => {
+        try {
+          const clients = await ts.clientList({client_type: 0})
+          clients.forEach(client => {
+            logger.info("发送消息", client.nickname)
+            client.message("Hello!")
+          })
+        } catch (e) {
+          console.log("出现错误!")
+          console.error(e)
+        }
+      })
+
+      ts.on("clientconnect", joinListener);
+      ts.on("close", closeListener);
+      ts.on("textmessage", TextMessageListener);
+
+    } catch (error) {
+      logger.error("连接到teamspeak服务器出错", error);
+    }
+    registerCommands(ctx, ts, logger);
+  });
+
+  ctx.on("dispose", () => {
+    if (!ts) return;
+    ts.removeAllListeners();
+    ts.quit().catch(error => logger.error("退出TeamSpeak连接时出错:", error));
+  });
 }
